d692e33e617384e292df5cbe9ad4627b7e7d146e
Hakan <99271598+Cluws@users.noreply.github.com>
2022-05-17 18:31:03 +0300

    emlak takip (yeni)

---
+++ WindowsFormsApp1/WindowsFormsApp1/login2.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class login2 : Form
+    {
+        MySqlConnection baglanti = new MySqlConnection(
+                "Server=localhost;Database=emlak;Uid=root;Pwd=12345;"
+                );
+        public login2()
+        {
+            InitializeComponent();
+
+
+            dataGridView1.ColumnCount = 4;
+            dataGridView1.Columns[0].Name = "name";
+            dataGridView1.Columns[1].Name = "surname";
+            dataGridView1.Columns[2].Name = "ev turu";
+            dataGridView1.Columns[3].Name = "adres";
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnemlak_Click(object sender, EventArgs e)
+        {
+            string isim = textBox1.Text;
+            string soyad = textBox2.Text;
+            string ev_turu = comboBox1.Text;
+            string adres = comboBox2.Text;
+            dataGridView1.Rows.Add(isim, soyad,ev_turu,adres);
+
+            try
+            {
+                MySqlCommand komut = new MySqlCommand();
+                komut.CommandText = "insert into emlak1(isim,soyadi,evturu,adres)" +
+                    "values('" + textBox1.Text + "','" + textBox2.Text + "','" + comboBox1.SelectedItem.ToString() + "','" + comboBox2.SelectedItem.ToString() + "');";
+                komut.Connection = baglanti;
+                komut.ExecuteNonQuery();
+                MessageBox.Show("Veri eklendi", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception hata)
+            {
+                MessageBox.Show("Hata meydana geldi. Hata : " + hata.Message.ToString());
+            }
+
+
+        }
+
+        private void login2_Load(object sender, EventArgs e)
+        {
+            baglanti.Open();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- emlak_Takip_CSharp_Forms-main/README.md
+++
@@ -1,8 +0,0 @@
-# CSharp Emlak Takip Programı
-
-![1_uhSX5djhrWiguXoWsW_lEw](https://user-images.githubusercontent.com/99271598/168275568-58b21dd9-ce3b-4a3b-80ae-9e9b7788e027.png)
-
-
-![indir](https://user-images.githubusercontent.com/99271598/168275960-c0ca4b03-818f-4459-a2ea-62ad15d55d89.png)
-- Emlak Takip Prgramını Hoş Geldiniz
-- Bu Projeyi Visual Studio 2019 ve MYSQL Workbench veri tabanı ile yaptık
